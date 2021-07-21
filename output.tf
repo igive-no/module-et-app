3b42914a6d86ac5a17013c7037518fa5cfc32279
Kristian Waagsbø <kristwa@gmail.com>
2021-07-21 02:15:07 +0200

    Fix output

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "azurerm_key_vault" {
-  value = "azurerm_key_vault.kv"
+  value = azurerm_key_vault.kv
 }
 
 output "azurerm_resource_group" {
-  value = "azurerm_resource_group.rg"
+  value = azurerm_resource_group.rg
 }
